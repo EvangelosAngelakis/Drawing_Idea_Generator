c2820bb17566190f9465c75636eea0cdca18286b
Angelakis <E.Angelakis@teledyne.com>
2025-02-21 10:45:59 +0000

    JS & CSS improvements
    Improved on the items list in the arrays in JS and the options in HTML.
    Made names stop going over the borders in CSS.
    Made an All in one HTML file to run on phones.

--- Drawing_Idea_Generator.js
+++ Drawing_Idea_Generator.js
@@ -1,14 +1,14 @@
 const selectedOptions = {
-    Animals: ["Lion", "Elephant", "Giraffe", "Dolphin", "Kangaroo", "Penguin", "Panda", "Eagle", "Shark", "Rabbit", "Wolf", "Crocodile", "Octopus", "Chimpanzee", "Flamingo", "Hippopotamus", "Cheetah", "Koala", "Turtle", "Peacock"],
+    Animals: ["Lion", "Elephant", "Giraffe", "Dolphin", "Kangaroo", "Penguin", "Panda", "Eagle", "Shark", "Rabbit", "Wolf", "Crocodile", "Octopus", "Chimpanzee", "Flamingo", "Whale", "Cheetah", "Koala", "Turtle", "Peacock"],
     Beasts: ["Dragon", "Griffin", "Minotaur", "Cerberus", "Hydra", "Chimera", "Kraken", "Manticore", "Phoenix", "Basilisk", "Werewolf", "Vampire", "Sphinx", "Gorgon", "Cyclops", "Yeti", "Leviathan", "Ogre", "Troll", "Wendigo"],
     SuperHeroes: ["Superman", "Batman", "Wonder Woman", "Spider-Man", "Iron Man", "Captain America", "Thor", "Hulk", "Black Panther", "Flash", "Aquaman", "Green Lantern", "Doctor Strange", "Scarlet Witch", "Wolverine", "Deadpool", "Shazam", "Cyborg", "Hawkeye", "Black Widow"],
-    Cute: ["Puppies", "Kittens", "Baby Pandas", "Bunnies", "Hedgehogs", "Ducklings", "Penguin Chicks", "Baby Elephants", "Stuffed Animals", "Tiny Hamsters", "Cupcakes", "Miniature Plants", "Cute Stickers", "Marshmallows", "Bubble Tea", "Chubby Birds", "Tiny Shoes", "Cartoon Characters", "Fluffy Clouds", "Smiling Suns"],
+    Treats: ["Cotton Candy", "Popcorn", "CupCakes", "Corn Dogs", "Candy Apples", "Churros", "Ice Cream", "Pretzels", "Snow Cones", "Mini Donuts", "Roasted Nuts", "Pizza", "Caramel Apples", "Corn on the cob", "Banoffee Pie", "Nachos", "Macarons", "Cannoli", "Slushies", "Choco Bananas"],
     Dinosaurs: ["Tyrannosaurus Rex", "Triceratops", "Stegosaurus", "Velociraptor", "Brachiosaurus", "Spinosaurus", "Ankylosaurus", "Pteranodon", "Allosaurus", "Diplodocus", "Iguanodon", "Pachycephalosaurus", "Parasaurolophus", "Carnotaurus", "Gallimimus", "Compsognathus", "Mosasaurus", "Quetzalcoatlus", "Megalosaurus", "Corythosaurus"],
     Flowers: ["Rose", "Tulip", "Sunflower", "Daisy", "Lily", "Orchid", "Peony", "Lavender", "Cherry Blossom", "Marigold", "Hibiscus", "Jasmine", "Daffodil", "Lotus", "Poppy", "Violet", "Bluebell", "Iris", "Magnolia", "Bougainvillea"],
     Fruits: ["Apple", "Banana", "Orange", "Grape", "Mango", "Pineapple", "Strawberry", "Watermelon", "Blueberry", "Peach", "Cherry", "Pomegranate", "Papaya", "Kiwi", "Lemon", "Lime", "Coconut", "Plum", "Fig", "Dragonfruit"],
-    Vegetables: ["Carrot", "Broccoli", "Spinach", "Potato", "Tomato", "Cucumber", "Bell Pepper", "Onion", "Garlic", "Pumpkin", "Lettuce", "Zucchini", "Cauliflower", "Peas", "Asparagus", "Beetroot", "Radish", "Eggplant", "Corn", "Mushroom"],
+    Vegetables: ["Carrot", "Broccoli", "Spinach", "Potato", "Tomato", "Cucumber", "Bell Pepper", "Onion", "Garlic", "Pumpkin", "Lettuce", "Courgette", "Cauliflower", "Peas", "Asparagus", "Beetroot", "Radish", "Aubergine", "Corn", "Mushroom"],
     Vehicles: ["Car", "Motorcycle", "Bicycle", "Truck", "Bus", "Train", "Airplane", "Helicopter", "Boat", "Ship", "Submarine", "Scooter", "Tractor", "Ambulance", "Fire Truck", "Police Car", "Taxi", "Camper Van", "Jet Ski", "Hot Air Balloon"],
-    Weapons: ["Sword", "Bow", "Arrow", "Dagger", "Axe", "Spear", "Mace", "Crossbow", "Gun", "Cannon", "Grenade", "Katana", "Nunchucks", "Flamethrower", "Slingshot", "Boomerang", "Whip", "Shuriken", "Hammer", "Scythe"]
+    Weapons: ["Sword", "Bow", "Arrow", "Dagger", "Axe", "Spear", "Mace", "Crossbow", "Gun", "Cannon", "Grenade", "Katana", "Nunchucks", "Flamethrower", "Slingshot", "Boomerang", "Staff", "Shuriken", "Hammer", "Scythe"]
 };
 
 let currentIndex = 0;
